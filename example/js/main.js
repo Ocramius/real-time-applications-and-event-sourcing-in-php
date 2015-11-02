e1da503ec1a9864578b8fe49d226911fecc99bc6
Marco Pivetta <ocramius@gmail.com>
2015-11-03 00:18:15 +0100

    Logic to create a poll

--- example/js/main.js
+++ example/js/main.js
@@ -1,7 +1,23 @@
-(function (document, fetch) {
+(function (window, document, fetch, CustomEvent) {
+    "use strict";
+
     var bowlingPlayground = document.querySelector('#bowling-playground'),
         createGame        = bowlingPlayground.querySelector('#new-game'),
         commandBus        = 'http://localhost:8888/command-bus.php';
+        /**
+         * creates a poll that isn't interval based, but waits for the previous poll to return a promise
+         */
+        createPoll        = function (callback, timeout) {
+            window.setTimeout(function () {
+                callback()
+                    .then(function () {
+                        createPoll(callback, timeout)
+                    })
+                    .catch(function () {
+                        createPoll(callback, timeout)
+                    });
+            }, timeout);
+        };
 
     createGame.addEventListener('click', function () {
         fetch(commandBus, {
@@ -9,4 +25,6 @@
             body: 'command=NewGame'
         });
     });
-}(document, fetch));
+
+    bowlingPlayground.dispatchEvent(new CustomEvent(''));
+}(window, document, fetch, CustomEvent));
