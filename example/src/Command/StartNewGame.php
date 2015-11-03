1c938b5cd516a9e4788003770d4ad541e4299ccf
Marco Pivetta <ocramius@gmail.com>
2015-11-03 02:32:45 +0100

    Command to start a new game

---
+++ example/src/Command/StartNewGame.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace ESBowling;
+
+final class StartNewGame
+{
+    private function __construct()
+    {
+    }
+
+    public static function fromPostData(array $postData)
+    {
+    }
+}
