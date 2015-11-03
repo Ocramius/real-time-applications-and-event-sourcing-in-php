f9ba71fb22bc461c44ec1cf72a325705cd321dcf
Marco Pivetta <ocramius@gmail.com>
2015-11-03 05:10:21 +0100

    Command bus initialization

--- example/command-bus.php
+++ example/command-bus.php
@@ -1,33 +1,46 @@
 <?php
 
-header('Content-Type: application/json'); // no custom headers in the demo, may cause issues
+use ESBowling\Command\StartNewGame;
+use ESBowling\Command\ThrowBall;
 
-if (strtolower($_SERVER['REQUEST_METHOD']) !== 'post') {
-    http_response_code(405);
+(function () {
+    header('Content-Type: application/json'); // no custom headers in the demo, may cause issues
 
-    echo json_encode([
-        'success' => false,
-        'message' => sprintf('Unsupported HTTP method %s', $_SERVER['REQUEST_METHOD']),
-    ]);
-
-    exit(1);
-}
-
-switch ($_POST['command'] ?? null) {
-    case 'NewGame':
-        break;
-
-    default:
-        http_response_code(400);
+    if (strtolower($_SERVER['REQUEST_METHOD']) !== 'post') {
+        http_response_code(405);
 
         echo json_encode([
             'success' => false,
-            'message' => sprintf('Unknown command %s requested', $_GET['command'] ?? null),
+            'message' => sprintf('Unsupported HTTP method %s', $_SERVER['REQUEST_METHOD']),
         ]);
 
         exit(1);
-}
+    }
 
-echo json_encode([
-    'success' => true,
-]);
+    /* @var $commandBus callable */
+    $commandBus = require __DIR__ . '/bootstrap-command-bus.php';
+
+    switch ($_POST['command'] ?? null) {
+        case 'StartNewGame':
+            $commandBus(StartNewGame::fromPostData());
+            break;
+        case 'ThrowBall':
+            $commandBus(ThrowBall::fromPostData($_POST));
+            break;
+
+        default:
+            http_response_code(400);
+
+            echo json_encode([
+                'success' => false,
+                'message' => sprintf('Unknown command %s requested', $_GET['command'] ?? null),
+            ]);
+
+            exit(1);
+    }
+
+    echo json_encode([
+        'success' => true,
+    ]);
+
+})();
