a53859e1ddb4458cecdf1d6e202016f69472fc93
Marco Pivetta <ocramius@gmail.com>
2015-11-03 02:26:04 +0100

    Basics of the command bus routing, better exception messages

--- example/command-bus.php
+++ example/command-bus.php
@@ -7,9 +7,25 @@ if (strtolower($_SERVER['REQUEST_METHOD']) !== 'post') {
 
     echo json_encode([
         'success' => false,
+        'message' => sprintf('Unsupported HTTP method %s', $_SERVER['REQUEST_METHOD']),
     ]);
 
     exit(1);
+}
+
+switch ($_POST['command'] ?? null) {
+    case 'NewGame':
+        break;
+
+    default:
+        http_response_code(400);
+
+        echo json_encode([
+            'success' => false,
+            'message' => sprintf('Unknown command %s requested', $_GET['command'] ?? null),
+        ]);
+
+        exit(1);
 }
 
 echo json_encode([
