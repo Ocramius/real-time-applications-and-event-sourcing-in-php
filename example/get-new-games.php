df621cfe1f4f4de067f80919ae636b9b4179486e
Marco Pivetta <ocramius@gmail.com>
2015-11-03 04:57:28 +0100

    Correcting logic to fetch new game events

--- example/get-new-games.php
+++ example/get-new-games.php
@@ -1,29 +1,30 @@
 <?php
 
-header('Content-Type: application/json'); // no custom headers in the demo, may cause issues
+(function () {
+    /* @var $repository \ESBowling\Repository\GameRepository */
+    $repository = require __DIR__ . '/bootstrap-repository.php';
 
-if (strtolower($_SERVER['REQUEST_METHOD']) !== 'get') {
-    http_response_code(405);
+    header('Content-Type: application/json'); // no custom headers in the demo, may cause issues
+
+    if (strtolower($_SERVER['REQUEST_METHOD']) !== 'get') {
+        http_response_code(405);
+
+        echo json_encode([
+            'success' => false,
+        ]);
+
+        exit(1);
+    }
 
     echo json_encode([
-        'success' => false,
+        'success' => true,
+        'games'   => array_values(array_map(
+            function (\ESBowling\DomainEvent\GameStarted $gameStarted) {
+                return $gameStarted->getGameId()->toString();
+            },
+            isset($_GET['lastGameId'])
+                ? $repository->getGameStartedEventsAfter(\Ramsey\Uuid\Uuid::fromString($_GET['lastGameId']))
+                : $repository->getAllGameStartedEvents()
+        )),
     ]);
-
-    exit(1);
-}
-
-$games = [
-    'abcdef',
-    'hijklm',
-];
-
-$gamesIndex = array_search(
-    $_GET['lastGameId'] ?? null,
-    $games,
-    true
-);
-
-echo json_encode([
-    'success' => true,
-    'games'   => array_slice($games, false === $gamesIndex ? 0 : $gamesIndex + 1),
-]);
+})();
